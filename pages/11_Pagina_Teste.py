e485c1f61db3150f2783b910e4dbbca36e0c2cb0
L-Honorato <165399440+L-Honorato@users.noreply.github.com>
2025-05-01 10:20:25 -0500

    avisos nas paginas

--- pages/11_Pagina_Teste.py
+++ pages/11_Pagina_Teste.py
@@ -11,21 +11,3 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-
-st.title("🦜 Observatório Curica")
-st.subheader("E̴͘͡s̷͘t̸̕͞a͡ ͢v̶̸͘e͠͏n̛͘͏d̶̡o͜?")
-#st.subheader("E͢͠͡s̷͞͞t̸̢̡a͢͡ ̡v̷́͞e̷̸̢n̛͝͞d̵̡͠ǫ̢͡?")
-st.markdown(
-    """
-    Bem-vindo ao Observatório Curica, uma iniciativa do grupo de pesquisa PoÉticas nas Amazônias,  
-    Grupo de Pesquisa Interdisciplinar em Linguagem e Educação, do Instituto Federal do Acre,  
-    em conjunto com a Promotoria Especializada de Defesa da Criança e do Adolescente de Cruzeiro do Sul-Acre.  
-      
-    O Observatório Curica é uma plataforma de visualização de dados para diagnóstico, monitoramento e intervenção  
-    nas políticas públicas educacionais do Estado do Acre e seus Municípios.       
-      
-    Utilize o menu lateral para navegar entre os panoramas e metas do Plano Nacional de Educação.
-    """,
-    unsafe_allow_html=True
-)
-
