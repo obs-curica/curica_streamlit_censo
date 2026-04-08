6a4cfdfad227f253a1a12c65b5bb5aa7d044d0f8
obs-curica <observatoriocurica@gmail.com>
2026-04-08 09:34:37 -0500

    correcao da funcao grafico_matriculas_por_dependencia_localizacao_municipio.

--- scripts/graficos.py
+++ scripts/graficos.py
@@ -650,8 +650,8 @@ def grafico_escolas_por_dependencia_localizacao(df, ano_censo):
     # Valores nas barras com espaçamento dinâmico
     espaco_texto = limite_superior * 0.015
     for i in range(len(categorias)):
-        ax.text(x[i] - largura/2, urbana[i] + espaco_texto, f'{urbana[i]:,.0f}', ha='center', color='white', fontweight='bold', fontsize=16)
-        ax.text(x[i] + largura/2, rural[i] + espaco_texto, f'{rural[i]:,.0f}', ha='center', color='white', fontweight='bold', fontsize=16)
+        ax.text(x[i] - largura/2, urbana.iloc[i] + espaco_texto, f'{urbana[i]:,.0f}', ha='center', color='white', fontweight='bold', fontsize=16)
+        ax.text(x[i] + largura/2, rural.iloc[i] + espaco_texto, f'{rural[i]:,.0f}', ha='center', color='white', fontweight='bold', fontsize=16)
 
     plt.tight_layout()
     st.pyplot(fig)
